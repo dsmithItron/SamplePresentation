d0f276cb94afd671d0943d99dded5a26d9b90819
Derek Smith <dersmith@itron.com>
2025-01-30 13:35:39 -0500

    added new test form templates

---
+++ testFormAspNetCore/testFormAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testFormAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
